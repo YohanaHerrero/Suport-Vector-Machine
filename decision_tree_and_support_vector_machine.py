b37af37b1a6e20787e93e30e52c1a01604c71f32
Yohana Herrero Alonso <81447012+YohanaHerrero@users.noreply.github.com>
2024-06-20 14:15:00 +0200

    Update decision_tree_and_support_vector_machine.py

--- decision_tree_and_support_vector_machine.py
+++ decision_tree_and_support_vector_machine.py
@@ -10,6 +10,10 @@ from snapml import DecisionTreeClassifier
 from sklearn.svm import LinearSVC
 from snapml import SupportVectorMachine
 from sklearn.metrics import hinge_loss
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import jaccard_score
+import itertools
 import time
 
 # download the dataset
@@ -117,6 +121,9 @@ sklearn_svm.fit(X_trainset, y_trainset)
 sklearn_time = time.time() - t0
 print("Scikit-Learn Training time (s):  {0:.2f}".format(sklearn_time))
 
+#predict 
+yhat = sklearn_svm.predict(X_testset)
+
 ##############################################  Support Vector Machine model with Snap ML  ##########################################################
 
 snapml_svm = SupportVectorMachine(class_weight='balanced', random_state=25, n_jobs=4, fit_intercept=False)
@@ -144,7 +151,7 @@ print("[Snap ML] ROC-AUC score:   {0:.3f}".format(acc_snapml))
 
 #the two models provide the same score on the test dataset. However, as in the case of decision trees, Snap ML runs the training routine faster than Scikit-Learn.
 
-##############################################  Another models evaluation  ##########################################################
+##############################################  Another models evaluation: hinge function  ##########################################################
 
 #evaluate the hinge loss from the predictions
 loss_snapml = hinge_loss(y_testset, snapml_pred)
@@ -156,7 +163,47 @@ print("Scikit-Learn Hinge loss:   {0:.3f}".format(loss_snapml))
 
 # the two models should give the same Hinge loss
 
+##############################################  Another models evaluation: confussion matrix  ##########################################################
+#Compute confusion matrix
+cnf_matrix = confusion_matrix(y_testset, yhat, labels=[0,1])
+np.set_printoptions(precision=2)
+print(classification_report(y_testset, yhat))
 
+#function to plot the confussion matrix
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix. Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
+    print(cm)
 
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Legal(0)','Fraudulent(1)'],normalize= False,  title='Confusion matrix')
+
+#I can also use the f1score or the jaccard index of accuracy
+print(f1_score(y_testset, yhat, average='weighted') )
+print(jaccard_score(y_testset, yhat,pos_label=0))
 
